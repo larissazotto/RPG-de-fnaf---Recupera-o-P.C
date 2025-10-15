2476f685eb28e95e2efdfb05b3a05f2c025b536d
larissazotto <larissa.zotto@escola.pr.gov.br>
2025-10-15 20:21:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const avanca = document.querySelectorAll('.btn-proximo');
+
+avanca.forEach(button => {
+    button.addEventListener('click', function(){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
+    })
+})
+//AQUI VOCÊ NÃO MEXE EM NADA NUNCA, MAS NÃO PODE ESQUECER DESSE ARQUIVO
